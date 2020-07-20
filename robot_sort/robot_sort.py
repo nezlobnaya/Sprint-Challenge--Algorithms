d8ee2d4bdf07722eddd01ca5b94ebcedbd98fdc4
nezlobnaya <vlad.burlutskiy@icloud.com>
2020-07-20 18:58:15 -0400

    minor refactoring by adding light methods to the solution

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -122,13 +122,17 @@ class SortingRobot:
         Sort the robot's list.
         """
         # Fill this out
-        while True:
+        self.set_light_on()
+        while self.light_is_on():
+            
             self.swap_item() # pick an item to start
+            self.set_light_off()
             while self.can_move_right() == True:
                 self.move_right()
                 
                 if self.compare_item() is not None and self.compare_item() == 1:
                     self.swap_item()
+                    self.set_light_on()
 
             while self.can_move_left():
                 self.move_left()
